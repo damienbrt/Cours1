179c9a55560fb4f3caf7b92b59cf39f9dc74f742
Damien BARTHE <damien.barthe@limayrac.fr>
2021-03-16 14:22:40 +0000

    push

--- lycee/models.py
+++ lycee/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
   first_name = models.CharField(
-    max_lenght = 50,
+    max_length = 50,
     blank = False,
     null = False
   )
@@ -12,4 +12,54 @@ class Student(models.Model):
     blank = False,
     null = False
   )
-  
+  last_name = models.CharField(
+    max_length = 255,
+    blank = False,
+    null = False,
+    help_text = "last name",
+    default= '???'
+  )
+  phone = models.CharField(
+    max_length = 10,
+    verbose_name = "phone number of student",
+    blank = False,
+    null = False,
+    help_text = "phone number of student",
+    default= '0123456789'
+  )
+  email = models.CharField(
+    max_length = 255,
+    verbose_name = "email of student",
+    blank = False,
+    null = False,
+    help_text = "email of student",
+    default = 'a@a.fr'
+  )
+  comment = models.CharField(
+    max_length = 255,
+    verbose_name = "email of student",
+    blank = False,
+    null = False,
+    help_text = "email of student",
+    default = 'a@a.fr'
+  )
+
+class Cursus(models.Model):
+  name = models.CharField(
+    max_length = 50,
+    blank = False,
+    null = True,
+    default = 'aucun'
+  )
+  year_from_bac = models.SmallIntegerField(
+    help_text = 'year since the bac',
+    verbose_name = 'year',
+    blank = False,
+    default = 0   
+  )
+  scholar_year = models.CharField(
+    max_length = 9,
+    blank = False,
+    null = True,
+    default= '0000-0001'
+  )
