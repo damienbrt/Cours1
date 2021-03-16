47215bc9d2548b848997a770ed1a1543d4b20f8a
Damien BARTHE <damien.barthe@limayrac.fr>
2021-03-16 15:56:16 +0000

    push migratiuon

--- lycee/models.py
+++ lycee/models.py
@@ -1,5 +1,27 @@
 from django.db import models
 
+class Cursus(models.Model):
+  name = models.CharField(
+    max_length = 50,
+    blank = False,
+    null = True,
+    default = 'aucun'
+  )
+  year_from_bac = models.SmallIntegerField(
+    help_text = 'year since the bac',
+    verbose_name = 'year',
+    blank = False,
+    default = 0   
+  )
+  scholar_year = models.CharField(
+    max_length = 9,
+    blank = False,
+    null = True,
+    default= '0000-0001'
+  )
+  def __str__(self):
+    return '{} {}'.format(self.name,self.scholar_year)
+
 # Create your models here.
 class Student(models.Model):
   first_name = models.CharField(
@@ -35,31 +57,16 @@ class Student(models.Model):
     help_text = "email of student",
     default = 'a@a.fr'
   )
-  comment = models.CharField(
+  comments = models.CharField(
     max_length = 255,
-    verbose_name = "email of student",
-    blank = False,
+    verbose_name = "comments of student",
+    blank = True,
     null = False,
-    help_text = "email of student",
-    default = 'a@a.fr'
+    help_text = "comments of student",
+    default = '...'
   )
-
-class Cursus(models.Model):
-  name = models.CharField(
-    max_length = 50,
-    blank = False,
-    null = True,
-    default = 'aucun'
-  )
-  year_from_bac = models.SmallIntegerField(
-    help_text = 'year since the bac',
-    verbose_name = 'year',
-    blank = False,
-    default = 0   
-  )
-  scholar_year = models.CharField(
-    max_length = 9,
-    blank = False,
-    null = True,
-    default= '0000-0001'
+  cursus = models.ForeignKey(
+    Cursus,
+    on_delete = models.CASCADE,
+    null = True
   )
